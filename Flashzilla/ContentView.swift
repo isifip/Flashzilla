b5e309c293ac699ffc84b74d195ac94fd93cdd9a
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-12 18:06:41 +0400

    Initial Commit

---
+++ Flashzilla/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Irakli Sokhaneishvili on 12.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
---
+++ Flashzilla/Model/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Irakli Sokhaneishvili on 12.01.22.
+//
+
+import Foundation
+
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who Played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
